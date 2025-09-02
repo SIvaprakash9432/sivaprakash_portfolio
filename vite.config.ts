350d670f893706fb1d1b7086058461089a6b3a01
SIvaprakash9432 <sivaprakash022003@gmail.com>
2025-09-02 09:17:13 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", 
+  base: "./", // ensures correct asset loading on Netlify
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // allows "@/..." imports
+    },
+  },
 })
